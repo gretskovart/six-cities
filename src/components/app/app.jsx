882fc9b24e14d529ed923daf22bf8e589e52bade
Artem <gretskovart@gmail.com>
2019-06-04 13:52:46 +0400

    add redux-logger

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {reducer, getData} from './../../reducer/reducer';
 import thunk from 'redux-thunk';
 import {compose} from 'recompose';
+import logger from 'redux-logger';
 
 import {configureAPI} from './../../api/api';
 import Main from './../main';
@@ -13,6 +14,7 @@ const store = createStore(
     reducer,
     compose(
         applyMiddleware(thunk.withExtraArgument(api)),
+        applyMiddleware(logger),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
